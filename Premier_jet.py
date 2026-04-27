068d36f2a31cacb9b728d35fab5df2313d1c8aa1
Awentir <thomas.corduan@gmail.com>
2026-04-27 15:58:52 +0200

    Passage Client vers Commands et ajout des principales commandes

--- Premier_jet.py
+++ Premier_jet.py
@@ -1,6 +1,8 @@
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+import discord
 import os
+import aiosqlite
 
 # Permet de charger notre Token
 os.getenv("TOKEN")
@@ -10,26 +12,52 @@ load_dotenv(dotenv_path="config.txt")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!",intents=intents)
 
-@client.event
+@bot.event
 # async def permet de ne pas bloquer le programme pendant que la fonction attend une "action"
 async def on_ready():
-    print(f'Connecté en tant que {client.user}')
+    async with aiosqlite.connect("taches") as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS globals_tasks(
+                id_tache INTEGER PRIMARY KEY,
+                id_user INTEGER,
+                name TEXT,
+                due TEXT,
+                priority INTEGER
+            )
+        """)
+        await db.commit() # Très important pour enregistrer les changements !
+        print("BDD prête !")
+    print(f'Connecté en tant que {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name='close')
+async def fermeture(ctx):
+    if ctx.author.name == 'awentir':
+        bot.close
+        print(f'{bot.user} a bien été déconnecté')
 
-    if message.content.startswith('$hello'):
-        # Await est essentiel, il permet qu'en attendant la permission de Discord pour envoyer le message de faire autre chose, il n'est donc pas gelé.
-        await message.channel.send('Hello!')
-    
-    if message.author.name == 'awentir':
-        if message.content == '!close':
-            print(f'{client.user} a bien été déconnecté')
-            client.close
+@bot.command(name='add')
+async def ajout(ctx,name,due,priority):
+    async with aiosqlite.connect("taches") as db:
+        await db.execute("""
+        INSERT INTO globals_tasks (id_user,name,due,priority) VALUES (?,?,?,?);
+        """,[ctx.author.id,name,due,priority])
+
+@bot.command(name='suppression')
+async def deleteBDD(ctx):
+    if ctx.author.id == '352375525282676736':
+        async with aiosqlite.connect("taches") as db:
+            await db.execute("""
+                DROP TABLE globals_tasks
+                """)
+
+@bot.command(name='tasks')
+async def affiche_tache(ctx):
+    tasks_user = discord.Embed(title='*_MES TÂCHES_*',colour=discord.Color.random())
+    await ctx.send(embed=tasks_user)
+
+
 
 # Token du bot discord
-client.run(os.getenv("TOKEN"))
+bot.run(os.getenv("TOKEN"))
